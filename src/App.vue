0884aa08ac5ce497829ecc9181fd4821918799ac
NicoMeier95 <78277049+NicoMeier95@users.noreply.github.com>
2022-10-01 11:06:32 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <SplitBillView />
+</template>
+
+<script setup>
+import SplitBillView from './components/SplitBillView.vue';
+
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter+Tight:wght@300;400;500;600&display=swap');
+*{
+  box-sizing: border-box;
+  margin:0;
+  padding: 0;
+  font-family: 'Inter Tight', sans-serif;
+}
+</style>
---
+++ src/components/SplitBillView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <InputForm />
+        <People />
+    </div>
+</template>
+
+<script setup>
+import InputForm from './InputForm.vue';
+import People from './People.vue';
+
+</script>
+
+<style>
+.container{
+    display: flex;
+    background-color: #1d1d1d;
+}
+</style>
